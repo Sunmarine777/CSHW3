baf974104fbbafa5be61e537548ee27629e6b42b
Sunmarine <Sunmarine777@yandex.ru>
2023-11-30 00:50:46 +0300

    Task3 diff found

---
+++ Task3/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 3: Задайте массив из вещественных чисел с ненулевой дробной 
+//частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+using System;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.Write("Enter array length: ");
+      int size = Convert.ToInt32(Console.ReadLine());
+
+        Random random = new Random();
+        double[] array = new double[size];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.NextDouble();
+            Console.WriteLine(array[i].ToString("F3"));
+        }
+
+        double max = 0;
+       
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] >= max)
+            max = array[i];
+        }
+
+         double min = max;
+                  
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] <= min)
+            min = array[i];
+        }
+
+        double diff;
+      
+        if (max >= min)
+        {
+            diff = max - min;
+        Console.WriteLine($"Difference is: " + diff.ToString("F3"));
+        }
+           else   
+            Console.WriteLine("No difference");
+
+      }
+}
